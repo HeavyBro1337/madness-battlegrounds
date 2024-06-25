10512ec66d17d3581f1d3f4e2f76a5cffb4c68d8
ivankorchmit <criperadmiral92@gmail.com>
2024-06-25 22:30:31 +0300

    commander camera

---
+++ src/commander/camera.rs
@@ -0,0 +1,32 @@
+use bevy::{
+    prelude::*,
+    prelude::{default, Camera3dBundle, Commands, Component},
+    render::camera::OrthographicProjection,
+};
+
+#[derive(Component)]
+pub struct CommanderCamera;
+
+const COMMANDER_POSITION: Vec3 = Vec3 {
+    x: 0.0,
+    y: 30.0,
+    z: 0.0,
+};
+
+pub fn setup_commander_camera(mut commands: Commands) {
+    use bevy::render::camera::Projection::Orthographic;
+
+    let projection = OrthographicProjection {
+        scaling_mode: bevy::render::camera::ScalingMode::FixedHorizontal(32.0),
+        ..default()
+    };
+
+    commands.spawn((
+        Camera3dBundle {
+            projection: Orthographic(projection),
+            transform: Transform::from_translation(COMMANDER_POSITION).looking_at(Vec3::X / 2.0, Vec3::Y),
+            ..default()
+        },
+        CommanderCamera,
+    ));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::WindowResolution,
+};
+use commander::camera::setup_commander_camera;
+
+pub mod commander;
 
 fn main() {
     App::new()
         .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
-
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Madness Battlegrounds".to_string(),
+                        resolution: WindowResolution::new(1024.0, 600.0),
+                        present_mode: bevy::window::PresentMode::Immediate,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup_commander_camera)
+        .run();
 }
